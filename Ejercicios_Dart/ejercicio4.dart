9abdf54eb18a7c100b85198ab627ac0f6e22d809
Yulieth_Vanegas <yvanegas2731@gmail.com>
2023-06-27 16:26:06 -0500

    Ejercicio Practica

---
+++ Ejercicios_Dart/ejercicio4.dart
@@ -0,0 +1,13 @@
+void main() {
+/* Realizar el siguiente ejercicio en Dart:
+un aprendiz recibe un subsidio base en el SENA,
+recibe una comision del 10% total de monitorias en investigacion
+con SENNOVA, se desea saber cuanto ganara  en un mes.*/
+
+  double subsidio = 1000000;
+  double comision = 0.10;
+  double porcentaje = subsidio * comision;
+  double ganancia = subsidio + porcentaje;
+  print('El aprendiz se gana al mes: $ganancia');
+}
+
